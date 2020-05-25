a32c0348bd3d1c6fc935e090e9db4ca5895629a7
nggt1 <tokotoko.ngg@gmail.com>
2020-05-25 17:50:56 +0530

    handlesubmit and handlechange functions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React, {Component} from 'react';
 import {v4 as uuidv4} from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
+//import "@fortawesome/fontawesome-free/css/all.min.css";
 import ItemList from './component/ItemList';
 import ItemInput from './component/ItemInput';
 //gitup setup
 class App extends Component{
   state = {
-    items : [{id:1, title:"desktop"},{id:2, title:"laptop"},
-    {id:3, title:"ipad"}],
+    items : [],
     id: uuidv4(),
     item: '',
     editItem:false
   };
 
-  handleChange =(e) =>{
-    console.log("handle change");
-  }
-  handleSubmit =(e) =>{
-    console.log("handle submit");
-  }
+  handleChange = e =>{
+    this.setState({
+      item: e.target.value
+    })
+  };
+
+  handleSubmit =e =>{
+    e.preventDefault();
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item
+    }
+    const updatedItems = [...this.state.items, newItem]
+    this.setState({
+      items:updatedItems,
+      item: '',
+      id: uuidv4,
+      editItem: false
+    },
+    ()=>console.log(this.state)
+    );
+  };
+
   clearList =() =>{
     console.log("clearList");
   }
