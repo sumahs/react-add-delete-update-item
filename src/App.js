205bb8325bf6160a1ecd5ae565526886d482645f
nggt1 <tokotoko.ngg@gmail.com>
2020-05-25 13:35:56 +0530

    showing vs-code github setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import uuid from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemList from './component/ItemList';
 import ItemInput from './component/ItemInput';
-
+//gitup setup
 class App extends Component{
     render(){
       return (
