272e75d68f0e1a6df6f13f557622a59f51d1d178
nggt1 <tokotoko.ngg@gmail.com>
2020-05-25 16:52:16 +0530

    add input

--- src/component/ItemInput.js
+++ src/component/ItemInput.js
@@ -2,9 +2,25 @@ import React, {Component} from 'react';
 
 export default class ItemInput extends Component{
     render(){
+        const{item, handleChange,handleSubmit,editItem} =this.props;
         return(
-            <div>Item input
-                
+            <div className="card card-body my-3">
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <div className="input-group-text bg-primary text-white">
+                                <i className="fas fa-book"></i>
+                            </div>
+                        </div>
+                        <input type="text" className="form-control text-capitalize"
+                            placeholder="add item"
+                            value={item}
+                            onChange={handleChange}/>
+                    </div>
+                    <button type="submit" className="btn btn-block btn-primary mt-3">
+                        Add Item
+                    </button>
+                </form>
             </div>
         );
     }
